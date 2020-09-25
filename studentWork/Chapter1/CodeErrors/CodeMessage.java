0688e5e7119a1b5093f34eb10fb30f07b7dbc1a5
Marysol Flores <marysol-f-ephrata-teals@outlook.com>
2020-09-25 17:08:09 +0000

    fix1

---
+++ studentWork/Chapter1/CodeErrors/CodeMessage.java
@@ -0,0 +1,8 @@
+package studentWork.Chapter1.CodeErrors;
+
+public class CodeMessage {
+    public static void main(String[] args) {
+        System.out.println("Speak friend");
+        System.out.println("and enter");
+    }
+}
